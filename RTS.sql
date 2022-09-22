f3bc0f57e9b47906985857c7434eee0fade88cc0
666godfist <kozlowskip347@gmail.com>
2022-09-22 11:05:25 +0200

    polecenia

---
+++ RTS.sql
@@ -0,0 +1,7 @@
+INSERT INTO players (nick, email, password) VALUES ("pawel", "siema123", "siema@gmail.com");
+INSERT INTO players (nick, email, password) VALUES ("oskar", "siema1234", "oskar321@gmail.com");
+INSERT INTO players (nick, email, password) VALUES ("kacper", "siema1235", "kacper123@gmail.com");
+INSERT INTO resources (name) VALUES ("drewno", "glina", "żelazo");
+INSERT INTO buildings (name) VALUES ("dół z gliny", "huta żelaza", "hata drwala");
+INSERT INTO troops (name) VALUES ("łucznik", "rycerz", "miecznik");
+SELECT name,password from players
